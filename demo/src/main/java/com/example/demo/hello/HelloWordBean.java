bc08936812b814210974c15a1b6026c6581febdb
norbert bautista <norbert.bautista.99@gmail.com>
2019-04-05 17:57:03 +0800

    work in progress backend

---
+++ demo/src/main/java/com/example/demo/hello/HelloWordBean.java
@@ -0,0 +1,27 @@
+package com.example.demo.hello;
+
+public class HelloWordBean {
+	
+	private String message;
+
+	public HelloWordBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWordBean [message=" + message + "]";
+	}
+	
+	
+	
+
+}
---
+++ demo/src/main/java/com/example/demo/hello/HelloWorldController.java
@@ -0,0 +1,29 @@
+package com.example.demo.hello;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping(path = "/hello-world")
+	public String helloWord() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWordBean helloWordBean() {
+	return new HelloWordBean("Hello World");
+		//throw new RuntimeException("Some Error has happened! in the backend please contact support at **");
+	}
+	
+	@GetMapping(path = "/hello-world-bean/path-variable/{name}")
+	public HelloWordBean helloWordBean(@PathVariable String name) {
+		return new HelloWordBean(String.format("Hello World, %s", name));
+	}
+	
+
+}
